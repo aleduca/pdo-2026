933f720c71f0ecde02c8789b65a60f020274982f
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2025-12-30 12:05:24 -0300

    Connection

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
 require '../vendor/autoload.php';
+
+try {
+	$dsn = 'mysql:host=localhost;dbname=blog_ci;charset=utf8mb4';
+	$pdo = new PDO($dsn, 'root', '', [
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	]);
+} catch (\PDOException $e) {
+	dd($e->getMessage());
+}

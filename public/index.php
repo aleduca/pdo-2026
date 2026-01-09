dbc69d50db1314b6222917126e993aceb76e2e81
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2026-01-09 16:05:59 -0300

    fetch modes e crud

--- public/index.php
+++ public/index.php
@@ -1,31 +1,36 @@
 <?php
 
-use app\entity\UserEntity;
-
 require '../vendor/autoload.php';
+
+if ($_SERVER['REQUEST_URI'] !== '/') {
+	exit;
+}
+
+file_put_contents(
+	__DIR__ . '/log.txt',
+	$_SERVER['REQUEST_URI'] . PHP_EOL,
+	FILE_APPEND
+);
 
 try {
 	$dsn = 'mysql:host=localhost;dbname=blog_ci;charset=utf8mb4';
 	$pdo = new PDO($dsn, 'root', '', [
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_BOTH,
 	]);
 	// PREPARED STATEMENT
 	// prepare\execute
-	$query = 'SELECT * FROM users where id = :id';
+	// CRUD - Create(INSERT), Read(SELECT), Update(UPDATE), Delete(DELETE)
+	$query = 'INSERT INTO users(firstName,lastName,email,password) VALUES(:firstName,:lastName,:email,:password)';
+	// $query = 'DELETE FROM users WHERE id = :id';
+	// $query = 'UPDATE users SET firstName = :firstName, lastName = :lastName, email = :email, password = :password WHERE id = :id';
 	$prepared = $pdo->prepare($query);
 	$prepared->execute([
-		'id' => 50,
+		'firstName' => 'Alexandre',
+		'lastName' => 'Cardoso',
+		'email' => 'email@email.com.br',
+		'password' => password_hash(123, PASSWORD_DEFAULT),
 	]);
-
-	// $prepared->setFetchMode(PDO::FETCH_CLASS, UserEntity::class);
-	$users = $prepared->fetch();
-	$data = UserEntity::fromArray($users);
-	dd($data);
-
-	// $userEntity = new UserEntity;
-	// $userEntity->age = 43;
-
-	dd($userEntity);
+	dd($pdo->lastInsertId());
 } catch (\Throwable $e) {
 	dd('Error ❌ ' . $e->getMessage());
 }

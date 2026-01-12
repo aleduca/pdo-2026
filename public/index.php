eea2576abe1187ea1c2c49b6b988e36edda23f0b
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2026-01-12 13:30:52 -0300

    transaction

--- public/index.php
+++ public/index.php
@@ -6,31 +6,53 @@ if ($_SERVER['REQUEST_URI'] !== '/') {
 	exit;
 }
 
-file_put_contents(
-	__DIR__ . '/log.txt',
-	$_SERVER['REQUEST_URI'] . PHP_EOL,
-	FILE_APPEND
-);
+// Banco te que ser do tipo InnoDB
+// Para mudar para innodb - ALTER TABLE your_table_name ENGINE = innodb;
+// Sem a transaction é auto commitado e com ele só o é quando chamo explicitamente o método commit
+
+// $pdo = null;
 
 try {
 	$dsn = 'mysql:host=localhost;dbname=blog_ci;charset=utf8mb4';
 	$pdo = new PDO($dsn, 'root', '', [
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_BOTH,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 	]);
-	// PREPARED STATEMENT
-	// prepare\execute
-	// CRUD - Create(INSERT), Read(SELECT), Update(UPDATE), Delete(DELETE)
-	$query = 'INSERT INTO users(firstName,lastName,email,password) VALUES(:firstName,:lastName,:email,:password)';
-	// $query = 'DELETE FROM users WHERE id = :id';
-	// $query = 'UPDATE users SET firstName = :firstName, lastName = :lastName, email = :email, password = :password WHERE id = :id';
-	$prepared = $pdo->prepare($query);
+	// inicia a transaction
+	$pdo->beginTransaction();
+	// Agora o inTransaction fica como true
+
+	// dd($pdo);
+
+	$pdo->query('DROP TABLE teste');
+
+	$prepared = $pdo->prepare('INSERT INTO users(firstName,lastName,email,password) VALUES(:firstName,:lastName,:email,:password)');
 	$prepared->execute([
 		'firstName' => 'Alexandre',
 		'lastName' => 'Cardoso',
 		'email' => 'email@email.com.br',
 		'password' => password_hash(123, PASSWORD_DEFAULT),
 	]);
-	dd($pdo->lastInsertId());
+
+	$pdo->query('DELETE FROM teste1');
+
+	$stmtUpdate = $pdo->prepare('UPDATE user SET firstName=:firstName, lastName=:lastName, email=:email, password=:password WHERE id=:id');
+	$stmtUpdate->execute([
+		'id' => 101,
+		'firstName' => 'Alexandre',
+		'lastName' => 'Eduardo Cardoso',
+		'email' => 'email@email.com.br',
+		'password' => password_hash(123, PASSWORD_DEFAULT),
+	]);
+
+	// confirma a transaction(transação), executa as queries.
+	$pdo->commit();
 } catch (\Throwable $e) {
+	// Rolls back a transaction - Faz rollback da transaction(transação)
+	if ($pdo instanceof PDO && $pdo->inTransaction()) {
+		dump('rolling back');
+		$pdo->rollBack();
+
+		// dd($pdo);
+	}
 	dd('Error ❌ ' . $e->getMessage());
 }
